6b9ca39d3856b5c57094a4dce967ec33dff82e00
profesor.asix <profesor.asix@gmail.com>
2015-02-05 14:14:21 +0100

    First commit

---
+++ src/daw/ed/uml/clock/ClockMain.java
@@ -0,0 +1,11 @@
+package daw.ed.uml.clock;
+/**
+ * @author carlos
+ */
+
+public class ClockMain {
+    public static void main(String [] args) {
+        Clock n = new Clock();
+    }
+    
+}
---
+++ test/daw/ed/uml/clock/ClockTest.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package daw.ed.uml.clock;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author carlos
+ */
+public class ClockTest {
+    
+    public ClockTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+    
+}
